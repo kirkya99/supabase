95639b8e4ee13114cd7981e0b1b4ca7749fa0d50
kirkya99 <fabianschotte@outlook.com>
2025-07-19 10:20:07 +0200

    supabase-db backup

--- docker/dump.sql
+++ docker/dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5d6d42c67b46d85d1d92c9d0f834796ccdf481c50891af90c1c68910837ac0c
-size 269732
+oid sha256:53c534eb780950f1b34b29591c3adc37151a23be534d2ecf01bdbd44aab18323
+size 343600
